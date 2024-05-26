21e5f9be9ef828c29bd06ee79b81ae10be51ff39
tanlosav <{ID}+{username}@users.noreply.github.com>
2024-05-26 13:01:05 +0300

    filter 'vendor' directory in todo-comments

--- .config/nvim/lua/tanlosav/plugins/todo-comments.lua
+++ .config/nvim/lua/tanlosav/plugins/todo-comments.lua
@@ -2,46 +2,35 @@ return {
   "folke/todo-comments.nvim",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local todo_comments = require("todo-comments")
-
-    todo_comments.setup({
-      colors = {
-        error = { "DiagnosticError", "ErrorMsg", "#FFFFFF" },
-        warning = { "DiagnosticWarn", "WarningMsg", "#FFFFFF" },
-        info = { "DiagnosticInfo", "#FFFFFF" },
-        hint = { "DiagnosticHint", "#FFFFFF" },
-        default = { "Identifier", "#FFFFFF" },
-        test = { "Identifier", "#FFFFFF" }
+  opts = {
+    search = {
+      command = "rg",
+      args = {
+        "--glob=!vendor/",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
       },
-      search = {
-        command = "rg",
-        args = {
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
-          "--glob=!vendor/",
-        },
-        -- regex that will be used to match keywords.
-        -- don't replace the (KEYWORDS) placeholder
-        pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-        -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
-      },
-    })
+    },
+  },
+  -- config = function(_, opts)
+  --   local todo_comments = require("todo-comments")
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+  --   todo_comments.setup(opts)
 
-    keymap.set("n", "]t", function()
-      todo_comments.jump_next()
-    end, { desc = "Next todo comment" })
+  --   -- set keymaps
+  --   local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "[t", function()
-      todo_comments.jump_prev()
-    end, { desc = "Previous todo comment" })
+  --   keymap.set("n", "]t", function()
+  --     todo_comments.jump_next()
+  --   end, { desc = "Next todo comment" })
 
-    todo_comments.setup()
-  end,
+  --   keymap.set("n", "[t", function()
+  --     todo_comments.jump_prev()
+  --   end, { desc = "Previous todo comment" })
+
+  --   todo_comments.setup()
+  -- end,
 }
