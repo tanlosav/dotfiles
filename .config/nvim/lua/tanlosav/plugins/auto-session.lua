df533210dbea5a7711d5c7c54daca801a3df9e88
tanlosav <{ID}+{username}@users.noreply.github.com>
2024-05-28 07:56:31 +0300

    disable auto-session keys

--- .config/nvim/lua/tanlosav/plugins/auto-session.lua
+++ .config/nvim/lua/tanlosav/plugins/auto-session.lua
@@ -10,7 +10,17 @@ return {
 
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
-    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+    -- keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
+    -- keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+    
+    -- require("which-key").register({
+    --   s = {
+    --         name = "Session",
+    --         r = { "<cmd>SessionRestore<CR>", "Restore session for cwd" },
+    --         s = { "<cmd>SessionSave<CR>", "Save session for auto session root dir" },
+    --       },
+    --   }, 
+    --   { prefix = "<leader>" }
+    -- )
   end,
 }
