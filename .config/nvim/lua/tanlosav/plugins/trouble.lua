7ff8b96cd5ab9453fba0c3d3efb5cc5a517274be
tanlosav <{ID}+{username}@users.noreply.github.com>
2024-08-18 22:29:25 +0300

    quickfix, telescope keybindings

--- .config/nvim/lua/tanlosav/plugins/trouble.lua
+++ .config/nvim/lua/tanlosav/plugins/trouble.lua
@@ -4,20 +4,6 @@ return {
   config = function()
     require('trouble').setup({})
 
-    -- require("which-key").register({
-    --   x = {
-    --         name = "Trouble",
-    --         f = { "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", "Buffer Diagnostics (Trouble)" },
-    --         l = { "<cmd>Trouble lsp toggle focus=false win.position=right<CR>", "LSP Definitions/references/... (Trouble)" },
-    --         L = { "<cmd>Trouble loclist toggle<CR>", "Location List (Trouble)" },
-    --         q = { "<cmd>Trouble qflist toggle<CR>", "Quickfix List (Trouble)" },
-    --         s = { "<cmd>Trouble symbols toggle pinned=true results.win.relative=win results.win.position=right<CR>", "Symbols (Trouble)" },
-    --         S = { "<cmd>Trouble lsp_document_symbols toggle pinned=true results.win.relative=win results.win.position=right<CR>", "Symbols (LSP)" },
-    --         x = { "<cmd>Trouble diagnostics toggle<CR>", "Diagnostics (Trouble)" },
-    --       },
-    --   }, 
-    --   { prefix = "<leader>" }
-    -- )
     require("which-key").add(
       {
         { "<leader>x", group = "Trouble" },
