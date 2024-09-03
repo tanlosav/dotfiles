dc4b8c8ee8f16963874bf406efbe0175474d8b24
tanlosav <{ID}+{username}@users.noreply.github.com>
2024-09-03 10:57:06 +0300

    fix telescope keybindings

--- .config/nvim/lua/tanlosav/plugins/telescope.lua
+++ .config/nvim/lua/tanlosav/plugins/telescope.lua
@@ -51,36 +51,56 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
+    -- <C-x>	Go to file selection as a split
+    -- <C-v>	Go to file selection as a vsplit
+    -- <C-u>	Scroll up in preview window
+    -- <C-d>	Scroll down in preview window
+    -- <C-f>	Scroll left in preview window
+    -- <C-k>	Scroll right in preview window
+    -- <M-f>	Scroll left in results window
+    -- <M-k>	Scroll right in results window
+    -- <C-/>	Show mappings for picker actions (insert mode)
+    -- ?	Show mappings for picker actions (normal mode)
+    -- <S-Tab>	Toggle selection and move to prev selection
+    -- <C-q>	Send all items not filtered to quickfixlist (qflist)
+    -- <M-q>	Send all selected items to qflist
+
     keymap.set('n', '<leader><leader>', "<cmd>Telescope oldfiles<CR>", {desc = 'Recent files'})
 
     require("which-key").add(
       {
         { "<leader>f", group = "Telescope" },
         { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Open buffers" },
-        { "<leader>ff", "<cmd>Telescope oldfiles<CR>", desc = "Recent files" },
         { "<leader>fg", group = "Git" },
         { "<leader>fgh", "<cmd>Telescope git_bcommits<CR>", desc = "Buffer's history" },
-        { "<leader>fj", "<cmd>Telescope jumplist<CR>", desc = "Jump list" },
-        { "<leader>fk", "<cmd>Telescope keymaps<CR>", desc = "Keymappings" },
         { "<leader>fl", group = "LSP" },
         { "<leader>flc", "<cmd>Telescope lsp_incoming_calls<CR>", desc = "Calls" },
         { "<leader>fld", "<cmd>Telescope lsp_definitions<CR>", desc = "Definitions" },
         { "<leader>fle", "<cmd>Telescope diagnostics bufnr=0<CR>", desc = "Diagnostics" },
         { "<leader>fli", "<cmd>Telescope lsp_implementations<CR>", desc = "Implementations" },
         { "<leader>flr", "<cmd>Telescope lsp_references<CR>", desc = "References" },
-        { "<leader>fls", "<cmd>Telescope lsp_document_symbols<CR>", desc = "Symbols" },
+        -- { "<leader>fls", "<cmd>Telescope lsp_document_symbols<CR>", desc = "Symbols" },
+        { "<leader>fls", function() require("telescope.builtin").lsp_document_symbols({symbols={"method","function","struct","constant"}}) end, desc = "Symbols" },
         { "<leader>flt", "<cmd>Telescope lsp_type_definitions<CR>", desc = "Type definition" },
-        { "<leader>fm", "<cmd>Telescope marks<CR>", desc = "Marks" },
-        { "<leader>fo", "<cmd>Telescope vim_options<CR>", desc = "Vim options" },
-        { "<leader>fr", "<cmd>Telescope registers<CR>", desc = "Registers" },
-        { "<leader>ft", "<cmd>TodoTelescope<CR>", desc = "Todo" },
-        { "<leader>fu", "<cmd>Telescope undo<cr>", desc = "Undo" },
         { "<leader>s", group = "Search" },
         { "<leader>sc", "<cmd>Telescope grep_string<CR>", desc = "Find string under cursor in cwd" },
         { "<leader>sf", "<cmd>Telescope find_files<CR>", desc = "Fuzzy find files in cwd" },
         { "<leader>sh", "<cmd>Telescope search_history<CR>", desc = "Search history" },
         { "<leader>ss", "<cmd>Telescope live_grep<CR>", desc = "Find string in cwd" },
-        { "<leader>fc", "<cmd>Telescope colorscheme<CR>", desc = "Schoose colorscheme" },
+        { "<leader>q", group = "Quickfix" },
+        { "<leader>qq", "<cmd>Telescope quickfix<CR>", desc = "Quickfix list" },
+        { "<leader>qh", "<cmd>Telescope quickfixhistory<CR>", desc = "Quickfix history" },
+        { "<leader>qw", "<cmd>copen<CR>", desc = "Quickfix window" },
+        { "<leader>ql", "<cmd>Telescope loclist<CR>", desc = "Location list" },
+        --
+        { "<leader>fc", "<cmd>Telescope colorscheme<CR>", desc = "Choose colorscheme" },
+        { "<leader>fj", "<cmd>Telescope jumplist<CR>", desc = "Jump list" },
+        { "<leader>fk", "<cmd>Telescope keymaps<CR>", desc = "Keymappings" },
+        { "<leader>fm", "<cmd>Telescope marks<CR>", desc = "Marks" },
+        { "<leader>fo", "<cmd>Telescope vim_options<CR>", desc = "Vim options" },
+        { "<leader>fr", "<cmd>Telescope registers<CR>", desc = "Registers" },
+        { "<leader>ft", "<cmd>TodoTelescope<CR>", desc = "Todo" },
+        { "<leader>fu", "<cmd>Telescope undo<cr>", desc = "Undo" },
       }
     )
   end,
