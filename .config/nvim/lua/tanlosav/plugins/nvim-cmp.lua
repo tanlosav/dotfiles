f59e21b93547f836af42eb7d0abdf70c951de792
tanlosav <{ID}+{username}@users.noreply.github.com>
2024-10-28 22:58:56 +0300

    update cmp settings

--- .config/nvim/lua/tanlosav/plugins/nvim-cmp.lua
+++ .config/nvim/lua/tanlosav/plugins/nvim-cmp.lua
@@ -24,15 +24,22 @@ return {
     require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,preview,noselect",
-        autocomplete = false,
-      },
+      -- completion = {
+      --   completeopt = "menu,menuone,preview,noselect",
+      --   autocomplete = false,
+      -- },
       snippet = { -- configure how nvim-cmp interacts with snippet engine
         expand = function(args)
-          luasnip.lsp_expand(args.body)
+          -- luasnip.lsp_expand(args.body) -- For `luasnip` users.
+          vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
         end,
       },
+      
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      
       mapping = cmp.mapping.preset.insert({
         ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
         ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
@@ -40,9 +47,10 @@ return {
         ["<C-d>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
       -- sources for autocompletion
+      
       sources = cmp.config.sources({
         { name = "nvim_lsp"},
         { name = "luasnip" }, -- snippets
@@ -58,5 +66,14 @@ return {
         }),
       },
     })
+
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+    
   end,
 }
