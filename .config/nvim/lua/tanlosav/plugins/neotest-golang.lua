23e8db9f72d780ec21ac0a7b2c8c0e03b9b802a9
tanlosav <{ID}+{username}@users.noreply.github.com>
2024-11-20 15:44:14 +0300

    disable quickfix in neotest-golang

--- .config/nvim/lua/tanlosav/plugins/neotest-golang.lua
+++ .config/nvim/lua/tanlosav/plugins/neotest-golang.lua
@@ -176,16 +176,16 @@ return {
               },
               status = { virtual_text = true },
               output = { open_on_run = true },
-              quickfix = {
-                open = function()
-                  if LazyVim.has("trouble.nvim") then
-                    require("trouble").open({ mode = "quickfix", focus = false })
-                  else
-                    vim.cmd("copen")
-                  end
-                end,
-                enabled = true,
-              },
+              -- quickfix = {
+              --   open = function()
+              --     if LazyVim.has("trouble.nvim") then
+              --       require("trouble").open({ mode = "quickfix", focus = false })
+              --     else
+              --       vim.cmd("copen")
+              --     end
+              --   end,
+              --   enabled = true,
+              -- },
         })
 
       require("which-key").add(
