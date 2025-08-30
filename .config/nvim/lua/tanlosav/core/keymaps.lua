0b6a4e050bcca2ccbcabb081f6d6197de34a6f90
tanlosav <{ID}+{username}@users.noreply.github.com>
2025-08-30 17:33:55 +0300

    blink config

--- .config/nvim/lua/tanlosav/core/keymaps.lua
+++ .config/nvim/lua/tanlosav/core/keymaps.lua
@@ -17,12 +17,10 @@ keymap.set("n", "<C>h", "<C-w>h", { desc = "Swith split to the left" })
 keymap.set("n", "<C>l", "<C-w>l", { desc = "Swith split to the right" })
 keymap.set("n", "<C>j", "<C-w>j", { desc = "Swith split to the down" })
 keymap.set("n", "<C>k", "<C-w>k", { desc = "Swith split to the up" })
-
--- keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
--- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
--- keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
--- keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
--- keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+-- keymap.set("n", "<M-h>", "<C-w>h", { desc = "Swith split to the left" })
+-- keymap.set("n", "<M-l>", "<C-w>l", { desc = "Swith split to the right" })
+-- keymap.set("n", "<M-j>", "<C-w>j", { desc = "Swith split to the down" })
+-- keymap.set("n", "<M-k>", "<C-w>k", { desc = "Swith split to the up" })
 
 -- scroll half page down and up, keeping the cursor centered
 keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll downwards' })
@@ -34,13 +32,17 @@ keymap.set("t", "<C-H>", "<C-\\><C-N><C-w>h", opts)
 keymap.set("t", "<C-J>", "<C-\\><C-N><C-w>j", opts)
 keymap.set("t", "<C-K>", "<C-\\><C-N><C-w>k", opts)
 keymap.set("t", "<C-L>", "<C-\\><C-N><C-w>l", opts)
+-- keymap.set("t", "<M-H>", "<C-\\><C-N><C-w>h", opts)
+-- keymap.set("t", "<M-J>", "<C-\\><C-N><C-w>j", opts)
+-- keymap.set("t", "<M-K>", "<C-\\><C-N><C-w>k", opts)
+-- keymap.set("t", "<M-L>", "<C-\\><C-N><C-w>l", opts)
 
 -- make U opposite to u (redo)
 keymap.set('n', 'U', '<C-r>', { desc = 'Redo' })
 
 -- word navigation in non-normal modes.
-keymap.set({ 'i', 'c' }, '<C-h>', '<C-Left>', { desc = 'Move word(s) backwards' })
-keymap.set({ 'i', 'c' }, '<C-l>', '<C-Right>', { desc = 'Move word(s) forwards' })
+-- keymap.set({ 'i', 'c' }, '<C-h>', '<C-Left>', { desc = 'Move word(s) backwards' })
+-- keymap.set({ 'i', 'c' }, '<C-l>', '<C-Right>', { desc = 'Move word(s) forwards' })
 
 -- prevent deleting from also copying
 vim.keymap.set({ 'n', 'v' }, 'd', '"_d', { noremap = true })
@@ -58,3 +60,8 @@ vim.keymap.set("n", "<BS>", "^", { desc = "Move to first non-blank character" })
 vim.keymap.set("n", "<M-j>", ":m .+1<CR>==", { desc = "Move line down" })
 vim.keymap.set("n", "<M-k>", ":m .-2<CR>==", { desc = "Move line up" })
 vim.keymap.set("n", "<C-r>", ":%s/", { desc = "Search and replace" })
+-- vim.keymap.set("n", "<M-r>", ":%s/", { desc = "Search and replace" })
+
+-- navigation
+keymap.set("n", "<M-]>", "<C-]>", { desc = "Go to tag" })
+keymap.set("n", "<M-[>", "<C-t>", { desc = "Pop tag stack" })
